b40af7f58f62de0d06704dce79287399faf070a3
Alex Lin <ardt4113c@gmail.com>
2018-09-19 11:57:00 +0800

    input

--- Project1/0516215_Project1.cpp
+++ Project1/0516215_Project1.cpp
@@ -1,10 +1,25 @@
 #include<iostream>
 using namespace std;
 
-int main(int argc, char const *argv[]){
-    int g;
+struct customer{
+    int arrive;
+    int rest;
+    int N;
+};
 
-    cin >> g;
+int main(int argc, char const *argv[]){
+    int g, customer_num;
+    struct customer cus[10000];
+
+    cin >> g >> customer_num;
+    for(int i = 0; i < customer_num; i++){
+        cin >> cus[i].arrive >> cus[i].rest >> cus[i].N;
+    }
+    
+
+
+
+
     cout << g+1 << endl;
     cout << argc << endl;
     return 0;
