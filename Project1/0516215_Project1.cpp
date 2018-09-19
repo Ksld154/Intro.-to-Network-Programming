6846469338e1acf5a8544d6d47b73c143e3d931c
Alex Lin <ardt4113c@gmail.com>
2018-09-19 16:17:24 +0800

    basic ideas(?

--- Project1/0516215_Project1.cpp
+++ Project1/0516215_Project1.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 struct customer{
     int arrive;
+    int continuous;
     int rest;
     int N;
 };
@@ -16,16 +17,41 @@ bool cmp(const customer A, const customer B){
 
 int main(int argc, char const *argv[]){
     int g, customer_num;
+    
     struct customer cus[1000];
 
     cin >> g >> customer_num;
+    const int G = g;
+
     for(int i = 0; i < customer_num; i++){
-        cin >> cus[i].arrive >> cus[i].rest >> cus[i].N;
+        cin >> cus[i].arrive >> cus[i].continuous >> cus[i].rest >> cus[i].N;
     }
+    sort(cus, cus+customer_num, cmp);
     
+    int finish_cnt = 0;
+    int t = 0;
+    t = cus[0].arrive;
     
-    while(1){
+    while(finish_cnt <= customer_num){
         
+        for(int i = 0; i < customer_num; i++){
+            int time_fin = t;
+            if(cus[i].N <= cus[i].continuous){
+                cout << t+cus[i].N << " " << i << " " << "finish playing YES" << endl;
+                g = G;
+            }
+            else if(g <= cus[i].continuous){
+                cout << t+g << " " << i << " " << "finish playing YES" << endl;
+                g = G;
+            }
+            else{
+                g -= cus[i].continuous;
+                cus[i].N -= cus[i].continuous;
+                //time_fin += cus[i].continuous;
+                cus[i].arrive += cus[i].continuous;
+            }
+        }
+        sort(cus, cus+customer_num, cmp);
     }
     
 
