45c57c49df505924798b4900599c6f9729b6276f
Alex Lin <ardt4113c@gmail.com>
2018-09-19 10:51:55 +0800

    establish files

---
+++ Project1/0516215_Project1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int g;
+
+    cin >> g;
+    cout << g+1 << endl;
+    cout << argc << endl;
+    return 0;
+}
+
