1db60475c0219dd0c3c99a7b28d841515f4ed99a
Alex Lin <ardt4113c@gmail.com>
2018-09-26 01:44:43 +0800

    start not fix

--- Project1/tmp.cpp
+++ Project1/tmp.cpp
@@ -90,71 +90,86 @@ int main(int argc, char const *argv[]){
 
 
 void *user(void *arg){
-    pthread_mutex_lock(&thread_switcher.mutex);
+    //pthread_mutex_lock(&thread_switcher.mutex);
+    pthread_mutex_lock(&machine.mutex_machine);
     struct customer	*cus_ptr = (struct customer *)arg;
     struct customer cus_info = *cus_ptr;
     
     while(1){
-
-        /*
+        int search_start = 0;
+        //int finish = 0;
+        
         //Using machine
         if(in_use && playing_id == cus_info.id){
             
-            pthread_mutex_lock(&machine.mutex_machine);
+            //pthread_mutex_lock(&machine.mutex_machine);
             cus_info.round_cnt++;
-            pthread_mutex_unlock(&machine.mutex_machine);
+            //pthread_mutex_unlock(&machine.mutex_machine);
 
             if(cus_info.N == cus_info.round_cnt || G == g_cnt){      //Finish playing, GET prize
 
-                printf("t=%d   id:%d    Finish playing YES\n", global_clock, cus_info.id);
+                printf("t=%2d   id:%d    Finish playing YES\n", global_clock, cus_info.id);
                 finish_cnt++;
                 in_use = 0;
-                //shared_stack.top--;
-
+                search_start = 1;
                 //pthread_mutex_unlock(&shared_stack.mutex);
                 pthread_exit(0);
             }else if(cus_info.round_cnt % cus_info.continuous == 0){  //Finish playing, did NOT get prize
 
-                printf("t=%d   id:%d    Finish playing NO\n", global_clock, cus_info.id);
+                printf("t=%2d   id:%d    Finish playing NO\n", global_clock, cus_info.id);
                 cus_info.arrive = global_clock + cus_info.rest;   //update the customer's new arrival time
                 pq.push(cus_info);
                 printf("pq_top = %d\n", pq.top().id);
-                in_use = 0; 
-                //shared_stack.top--;
-                //pthread_mutex_unlock(&shared_stack.mutex);
+                in_use = 0;
+                search_start = 1; 
             }
         }
 
-        //Wait for machine
-        if(in_use && (playing_id != cus_info.id) && (cus_info.arrive <= global_clock) && !cus_info.waiting){
-            //pthread_mutex_lock(&shared_stack.mutex);
-            
-            printf("t=%d   id:%d    Waiting \n", cus_info.arrive, cus_info.id);
-            cus_info.waiting = 1;
-            //printf("t=%d   id:%d  wait:%d    Waiting \n", cus_info.arrive, cus_info.id, cus_info.waiting);
 
-            //pthread_mutex_unlock(&shared_stack.mutex);
-        }
-
-        //Start playing
+        //Start playing(BUG)
         if(!in_use && !pq.empty() && pq.top().id == cus_info.id && cus_info.arrive <= global_clock){    
             pq.pop();
-            pthread_mutex_lock(&machine.mutex_machine);
             playing_id = cus_info.id;
             in_use = 1;
-            pthread_mutex_unlock(&machine.mutex_machine);
-            
-            printf("t=%d   id:%d    Start playing\n", global_clock, cus_info.id);
+            printf("t=%2d   id:%d    Start playing\n", global_clock, cus_info.id);
         }
-        */
+        else if(!in_use && !pq.empty() && pq.top().arrive <= global_clock && search_start == 1){
+            printf("t=%2d   id:%d    Start playing1\n", global_clock, pq.top().id);
+            playing_id = pq.top().id;           
+            pq.pop();
+            in_use = 1;
+            search_start = 0;
+        }
+        
+
+
+        //Wait for machine(BUG)
+        if(in_use && (playing_id != cus_info.id) && (cus_info.arrive <= global_clock) && !cus_info.waiting){ 
+            printf("t=%2d   id:%d    Waiting \n", cus_info.arrive, cus_info.id);
+            cus_info.waiting = 1;
+
+        }else if(!in_use && cus_info.id != pq.top().id && (cus_info.arrive <= global_clock) && !cus_info.waiting){  //the machine is IDLE, but there are multiple users that can use it immediatly,
+            printf("t=%2d   id:%d    Waiting \n", cus_info.arrive, cus_info.id);
+            // only the man who is the first one in the queue can use it, others must to wait 
+            cus_info.waiting = 1;
+        }
+
+
+
+
+        
         //Early quit (for test)
         if(global_clock == 40){
 			finish_cnt++;
 			cout << cus_info.id << endl;
 			(void) pthread_mutex_unlock(&thread_switcher.mutex);
 			pthread_exit(0);
-		}	
-        
+		}
+
+
+        pthread_mutex_unlock(&machine.mutex_machine);
+
+        pthread_mutex_lock(&thread_switcher.mutex);
         printf("Customer_id:%d   Thread_cnt:%d   Clock=%d\n", cus_info.id, thread_cnt, global_clock);
         thread_cnt++;
         pthread_cond_wait(&thread_switcher.child_thread, &thread_switcher.mutex);
