1be62e3cb0bb61c86669ae742c0be0f471e9166b
Alex Lin <ardt4113c@gmail.com>
2018-10-10 02:46:32 +0800

    need to finsih tokenlist and parser

--- Project2/0516215_Project2.cpp
+++ Project2/0516215_Project2.cpp
@@ -7,76 +7,155 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<json-c/json.h>
+#include<list>
+using namespace std;
 
 #define MAX_LINE 200
+
+
+
+struct user_token{
+    char    id[50];
+    char    token[100];
+};
+
+list<struct user_token> TokenList;
+
+
+
+void JsonParser(char *receive_msg, char UserID){
+    json_object *msg_obj, *status;       // essential column
+    json_object *message, *token, *post, *invite, *friends;  // optional column
+    msg_obj = json_tokener_parse(receive_msg);
+
+    //printf("new_obj.to_string()=\n%s\n", json_object_to_json_string(new_obj));
+
+    status  = json_object_object_get(msg_obj, "status");
+    message = json_object_object_get(msg_obj, "message");
+    token   = json_object_object_get(msg_obj, "token");
+
+    // UPDATE TOKENLIST:
+    // if the json contains "token" column(i.e. it is a login command), 
+    // then update the token list 
+    if(token){
+        char *token_string = json_object_get_string(token);
+        struct user_token CurrentUser = {UserID, token_string};
+        TokenList.push_back(CurrentUser);
+    }
+
+
+    // If parse fail, object is NULL
+    if (!status) {
+        printf("parse failed.");
+        json_object_put(msg_obj);
+        return;
+    }
+
+    if(message)
+        printf("%s\n", json_object_get_string(message));
+    else if(friends){
+        // do something (print the vector)    
+    }
+    return;
+}
+
+
+
+
+
 
 //server IP:    140.113.207.51
 //server port:  8008
 
 int main(int argc, char const *argv[]){
-    FILE *fPtr;
-    char InputBuffer[MAX_LINE];
-    char ReceiveMsg[500];
-    
-    memset(InputBuffer, '\0', sizeof(InputBuffer));
-    read(STDIN_FILENO, InputBuffer, MAX_LINE);
+    //list<struct user_token> TokenList;
 
+    while(1){
 
-    /*create a socket*/
-    int sockfd;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd == -1){
-        printf("Fail to create a socket.\n");
-    }
+        //FILE    *fPtr;
+        char    InputBuffer[MAX_LINE];
+        char    ReceiveMsg[500];
+        char    *arg[MAX_LINE/2];
+        char    *CommandType = NULL;
+        char    *UserID = NULL;
 
-    /*create connection to server*/
-    struct sockaddr_in serv;
-    memset(&serv, 0, sizeof(serv)); 
-    serv.sin_family = PF_INET;
-    serv.sin_addr.s_addr = inet_addr("140.113.207.51");
-    serv.sin_port = htons(8008);
+        //read input from stdin
+        memset(InputBuffer, '\0', sizeof(InputBuffer));
+        read(STDIN_FILENO, InputBuffer, MAX_LINE);
+        strtok(InputBuffer, "\n");
+        
+        /*slice the input command*/
+		arg[0] = strtok(InputBuffer," ");
+        char *tmp = arg[0];
+		for(int i = 1; tmp != NULL ;i++){
+			arg[i] = strtok(NULL, " ");
+			tmp = arg[i];
+		}
 
-    int con_err = connect(sockfd, (struct sockaddr *)&serv, sizeof(serv));
-    if(con_err == -1){
-        printf("Connection fail\n");
-    }
+        CommandType = arg[0];
+        UserID = arg[1];
 
-    send(sockfd, InputBuffer, sizeof(InputBuffer), 0);
-    recv(sockfd, ReceiveMsg,  sizeof(ReceiveMsg),  0);
-    close(sockfd);
-    //printf("%s\n", ReceiveMsg);
+        printf("Type: %s\n", CommandType);
+        // CHECK COMMAND TYPE: 
+        // if the command is not a Login nor a Register command, 
+        // then replace the "id" argument into the user's "token" 
+        if((strcmp(CommandType, "login") != 0) && (strcmp(CommandType, "register") != 0) ){
+            bool foundToken = 0;
+            list<struct user_token>::iterator it;
+            for(it = TokenList.begin(); it != TokenList.end(); it++){
+                // the user's id is in the token list (i.e. the user has already login)
+                if(it->id == arg[1]){     
+                    foundToken = 1;
+                    arg[1] = it->token;
+                    break;
+                }    
+            }
+            
+            // the user hasn't login, then replace his id with an empty string
+            if(foundToken == 0)
+                arg[1] = NULL;
+            
+            // do something else?
+        }
 
-    json_object *new_obj, *status, *message;
-
-    new_obj = json_tokener_parse(ReceiveMsg);
-    //printf("new_obj.to_string()=\n%s\n", json_object_to_json_string(new_obj));
-
-
-    status  = json_object_object_get(new_obj, "status");
-    message = json_object_object_get(new_obj, "message");
-    // If parse fail, object is NULL
-    if (!status || !message) {
-        printf("parse failed.");
-        json_object_put(new_obj);
-        exit(-1);
-    }
-
-    // Fetch value
-    //printf("name=%s", json_object_get_string(name));
-    printf("%s\n", json_object_get_string(message));
-
-
-
+        int BufferLen = strlen(InputBuffer);
+        char SendMsg[BufferLen];
+        memset(SendMsg, '\0', sizeof(SendMsg));
+        memcpy(SendMsg, InputBuffer, BufferLen); // copy data from old buf to new one
 /*
-    fPtr = fopen("recv.json", "w");
-    if (!fPtr) {
-        printf("Fail to create a json file.\n");
-        exit(1);
+        //create a socket
+        int sockfd;
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if(sockfd == -1){
+            printf("Fail to create a socket.\n");
+        }
+
+        //create connection to server
+        struct sockaddr_in serv;
+        memset(&serv, 0, sizeof(serv)); 
+        serv.sin_family = PF_INET;
+        serv.sin_addr.s_addr = inet_addr("140.113.207.51");
+        serv.sin_port = htons(8008);
+
+        int con_err = connect(sockfd, (struct sockaddr *)&serv, sizeof(serv));
+        if(con_err == -1){
+            printf("Connection fail\n");
+        }
+        
+        // deal with<user> : replace <id> with token
+
+
+
+
+
+        send(sockfd, SendMsg, sizeof(SendMsg), 0);
+        recv(sockfd, ReceiveMsg, sizeof(ReceiveMsg), 0);
+        close(sockfd);
+
+
+        JsonParser(ReceiveMsg, Userid);
+
+*/
     }
-    fprintf(fPtr, "%s", ReceiveMsg);
-    fclose(fPtr);
-*/     
-
-
     return 0;
 }
