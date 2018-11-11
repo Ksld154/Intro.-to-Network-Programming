23bf70a1d19405a3fdcdc826957f79e0d55523db
Alex Lin <ardt4113c@gmail.com>
2018-11-11 18:10:36 +0800

    1111 1810

--- Project3/0516215_Project3.py
+++ Project3/0516215_Project3.py
@@ -44,17 +44,24 @@ class User(SQLiteModel):
     def Login_User(self, account, password):
         try:
             #(1) do query
-            query = User.select().where( (User.username == account) & (User.password == password) )
+            query = User.select(User.id).where( (User.username == account) & (User.password == password) )
             if len(query) == 1:
                 print("Login success!!\n\n")
+                print(query[0])
+                
                 #print("%s %s" %(query[0].username, query[0].password))
                 
                 #(2) generate token (maybe use username + timestamp + hash function)
                 token = hashlib.sha256((str(time.time())+account).encode('utf-8')).hexdigest()
-                print(token)
 
                 #(3) then update token list
-                TokenList.create(username=account, token=token)
+
+                
+                TokenList.create(user_id=query[0], token=token)
+                
+                
+                
+                
                 json_file = json.dumps({'status':0, 'token':token, 'message': 'Success!'})
                 return json_file
 
@@ -65,10 +72,17 @@ class User(SQLiteModel):
 
         except pw.IntegrityError as e:
             print(e)
-            
+    
+    @classmethod
+    def Delete_User(self, token):
+        # do it later
+        # need to do cascade delete, so wait for me to design other tables
+        pass
+
 
 class TokenList(SQLiteModel):
-    username = pw.CharField(unique=True)
+    #username = pw.CharField(unique=True)
+    user_id = pw.ForeignKeyField(User, on_update='CASCADE', on_delete='CASCADE')
     token = pw.CharField()
 
     @classmethod
@@ -76,23 +90,72 @@ class TokenList(SQLiteModel):
         try:
             query = TokenList.delete().where(TokenList.token == token)
             query.execute()
-            #TokenList().delete_instance().where(TokenList.token == token)
 
-            
-            
-            print(token)
-            # data = [{'status':0, 'message': 'Bye!'}]
             json_file = json.dumps({'status':0, 'message': 'Bye!'})
             return json_file
 
         except pw.IntegrityError as e:
             print(e)
 
-    #def Search_Token(self, account):
+    @classmethod
+    def Search_Token(self, account):
+        pass
 
 
 
+class Invite(SQLiteModel):
+    from_id = pw.ForeignKeyField(User, on_update='CASCADE', on_delete='CASCADE')
+    to_id   = pw.ForeignKeyField(User, on_update='CASCADE', on_delete='CASCADE')
+    
+    @classmethod
+    def InviteFriend(self, from_token, to_username):
+        try:
+            #query = TokenList.delete().where(TokenList.token == token)
+            #query.execute()
+            
+            q1 = TokenList.select(TokenList.user_id).where(TokenList.token == from_token)
+            from_id = q1[0]
+            print(from_id) # query[0] is user id
+            
+            q2 = User.select(User.id).where(User.username == to_username)
+            if len(q2) == 1:
+                to_id = q2[0]
+                print(to_id)
 
+                
+                if from_id == to_id:
+                    return json.dumps({'status':1, 'message': 'You cannot invite yourself'})                
+
+                q3 = Invite.select().where((Invite.from_id == from_id) & (Invite.to_id == to_id))
+                if len(q3) != 0:
+                    return json.dumps({'status':1, 'message': '​Already invited'})
+
+                q4 = Invite.select().where((Invite.from_id == to_id) & (Invite.to_id == from_id))
+                if len(q4) != 0:
+                    return json.dumps({'status':1, 'message': '​<id> has invited you'})
+
+                # check: ​“<id>​is already your friend”
+                q5 = Friend.select().where((Friend.friendA_id == from_id) && (Friend.friendB_id == to_id))
+                if len(q5) != 0:
+                    return json.dumps({'status':1, 'message': '<id>​is already your friend'})
+
+                
+                # if no problem, then insert into invite list
+                Invite.create(from_id=from_id, to_id=to_id)
+                return json.dumps({'status':0, 'message': 'Success!'})
+            
+            elif len(q2) == 0:
+                json_file = json.dumps({'status':1, 'message': '​<id>​does not exist'})
+                return json_file
+
+        except pw.IntegrityError as e:
+            print(e)
+
+
+class Friend(SQLiteModel):
+    friendA_id = pw.ForeignKeyField(User, on_update='CASCADE', on_delete='CASCADE')
+    friendB_id = pw.ForeignKeyField(User, on_update='CASCADE', on_delete='CASCADE')
+    pass
 
 
 
@@ -159,10 +222,27 @@ def main():
                     json_file = json.dumps({'status':1, 'message': "Not login yet"})
                 else:
                     json_file = json.dumps({'status':1, 'message': "Usage: logout​<user>"})
-                                 
+            
+            
+            
+            
+            # not finished!!!
+            elif command[0] == "delete":
+                if len(command) == 2:
+                    pass
+                    #json_file = User.Delete_User(command[1])
+                elif len(command) < 2:
+                    json_file = json.dumps({'status':1, 'message': "Not login yet"})
+                else:
+                    json_file = json.dumps({'status':1, 'message': "Usage: delete​<user>"})             
 
-
-            ServMsg = "Server received."
+            elif command[0] == "invite":
+                if len(command) == 3:
+                    pass
+                elif len(command) == 2:
+                    json_file = json.dumps({'status':1, 'message': "Not login yet"})
+                else:
+                    json_file = json.dumps({'status':1, 'message': "Usage: invite​<user>​​<id>"})
 
             print(json_file)
             msg_byte = json_file.encode('utf-8')
